dc8ecc2031bb2b667b96d4e7d9cce365fa53dd3a
Isabela Cardoso <82220569+Isabela-Cardoso@users.noreply.github.com>
2021-05-17 16:38:30 -0300

    Update rotaApi.js

--- src/routes/rotaApi.js
+++ src/routes/rotaApi.js
@@ -6,17 +6,25 @@ var planetaController = require('../controllers/planetasController');
 
 //listando planetas
 router.route('/planetas').get(planetaController.index);
+console.log('Lista ok')
 
 //deletando planetas por id
 router.route('/planetas/id/:planeta_id').get(planetaController.delete);
+console.log('Delete por ID ok')
+
 
 //adicionando os planetas
 router.route('/planetas/adicionar').post(planetaController.view);
+console.log('Planetas a adicionar ok')
 
 //encontrando planet por nome
 router.route('/planetas/nome/:nome').get(planetaController.v);
+console.log('Encontrando por ID ok')
 
 //deletando planeta por is
 router.route('/planetas/id/deletar/:planeta_id').delete(planetaController.delete);
+console.log('Delete ok')
+
 
 module.exports = router;
+
